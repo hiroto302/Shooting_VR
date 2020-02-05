b61a542b52e3b7e97f29d3e47c12a18371c07313
hiroto302 <i.am.a.hiroto@gmail.com>
2020-02-05 11:38:38 +0900

    ゾンビの移動
    プレイヤーに向かって移動してくる

---
+++ Assets/Scripts/FolowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FolowPlayer : MonoBehaviour
+{
+    NavMeshAgent agent;
+    GameObject player;  //プレイヤー視点のカメラを指す
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindWithTag("Player");  //タグ識別によるオブジェクト検索
+    }
+
+    void Update()
+    {
+        agent.destination = player.transform.position;  //playerの位置にゾンビが向かっていく
+    }
+}
---
+++ Assets/Scripts/ZombiController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombiController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Attack()
+    {
+        //攻撃するアニメーション
+
+        //ゲームオーバー画面に移動
+    }
+}
